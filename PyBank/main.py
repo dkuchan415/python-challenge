3d97440011b09135dc5f208e9eab5dcb8cc11ce6
dkuchan415 <danielkuchan@yahoo.com>
2020-09-22 22:29:41 -0500

    Final submission

--- PyBank/main.py
+++ PyBank/main.py
@@ -19,7 +19,6 @@ with open(csvpath) as csvfile:
 
     # Read the header row first (skip this step if there is now header)
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
 
     # Read each row of data after the header
     net_profit = 0
@@ -69,3 +68,4 @@ with open(csvpath) as csvfile:
 
     # The greatest decrease in losses (date and amount) over the entire period
 
+    print(f"Greatest Decrease in Profits: ")
